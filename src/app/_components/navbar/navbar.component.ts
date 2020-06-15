dfa22256c3c0331a8f3c5839fc07c054163447fc
Hannes Rüger <hannesrueger@gmx.de>
2020-06-15 15:49:55 +0200

    fixed navbar toggle button on mobile

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -7,12 +7,14 @@ import { ViewportScroller } from '@angular/common';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public showMenu: boolean = false;
   constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
   public goToProjects() {
+    this.showMenu = false; 
     this.viewportScroller.scrollToAnchor('projects');
   }
 
