69ffb33999984d6e794dd1fc71eb00d27349799b
Hannes Rüger <hannesrueger@gmx.de>
2020-06-15 14:23:37 +0200

    fixed navbar projects link not working

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+  }
+
+  public goToProjects() {
+    this.viewportScroller.scrollToAnchor('projects');
   }
 
 }
