7582c71aa4bc4a4550b860a6ea9faa9e05862998
Hannes Rüger <hannesrueger@gmx.de>
2020-06-15 14:21:41 +0200

    new header

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+  }
+
+  public goToProjects() {
+    this.viewportScroller.scrollToAnchor('projects');
   }
 
 }
